bd7d087e91b667fbc22318e346ada29d4609bbb5
Moshfiqur Rahman <studentemam@gmail.com>
2025-01-06 15:37:41 +0600

    feat(user): add Mongoose user schema with strict validation

---
+++ src/app/modules/User/user.model.ts
@@ -0,0 +1,47 @@
+import { model, Schema } from "mongoose";
+import { TUser } from "./user.interface";
+
+const userSchema = new Schema<TUser>({
+    firstName: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+    lastName: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+    email: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+    role: {
+        type: Schema.Types.String,
+        enum: ["user", "doctor", "accounts-specialist", "finance-manager", "admin", "super-admin"],
+        default: "user",
+    },
+    status: {
+        type: Schema.Types.String,
+        enum: ["active", "suspend"],
+        default: "active",
+    },
+    isDeleted: {
+        type: Boolean,
+        default: false,
+    }
+},
+    {
+        timestamps: true,
+        versionKey: false, // disables the '__v' field
+    }
+)
+
+export const User = model<TUser>("User", userSchema);
