7d4cf0bafb1962d3fb76da2ca18a2ddc2eb6b2da
Moshfiqur Rahman <studentemam@gmail.com>
2025-01-06 15:13:12 +0600

    feat(user): define TUser type for user

---
+++ src/app/modules/User/user.interface.ts
@@ -0,0 +1,9 @@
+export type TUser = {
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+    role: "user" | "doctor" | "accounts-specialist" | "finance-manager" | "admin" | "super-admin";
+    status: "active" | "suspend";
+    isDeleted: boolean;
+}
