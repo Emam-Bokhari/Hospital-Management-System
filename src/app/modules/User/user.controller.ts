1cfae139f7118879857407afffa98e5f6d636e2e
Moshfiqur Rahman <studentemam@gmail.com>
2025-01-07 13:56:58 +0600

    refactor(user): use sendResponse utility for standardized response

--- src/app/modules/User/user.controller.ts
+++ src/app/modules/User/user.controller.ts
@@ -1,15 +1,16 @@
 import { Request, Response } from "express";
 import { UserServices } from "./user.service";
+import { sendResponse } from "../../utils/sendResponse";
 
 const createUserController = async (req: Request, res: Response) => {
     try {
         const userPayload = req.body;
         const createdUser = await UserServices.createUser(userPayload);
 
-        res.send({
-            success: true,
-            message: "User successfully registered",
+        sendResponse(res, {
             statusCode: 201,
+            success: true,
+            message: "User registered successfully",
             data: createdUser,
         })
     } catch (err) {
