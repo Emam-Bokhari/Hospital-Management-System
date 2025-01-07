ac71f46418c4ea319b8ca2ab637cabc56fe3829f
Moshfiqur Rahman <studentemam@gmail.com>
2025-01-07 14:39:44 +0600

    feat(routes): implement central routing

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,17 @@
 import express from "express";
 import cors from "cors";
+import router from "./app/routes";
 
 
 const app = express();
 
+
 // middleware
 app.use(express.json());
 app.use(cors());
+
+// application routes
+app.use("/api/v1/", router)
 
 // check health
 app.get("/", (req, res) => {
