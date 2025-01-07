e55b3b0ba06058a1859a1ddb4f918d83a81e91ba
Moshfiqur Rahman <studentemam@gmail.com>
2025-01-07 21:15:10 +0600

    feat: add Zod validation error handling with formatted response

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,18 +1,30 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { NextFunction, Request, Response } from "express";
 import config from "../config";
 import express from "express";
+import { TError } from "../interface/error";
+import { ZodError } from "zod";
+import { handleZodValidationError } from "../errors/handleZodValidationError";
 
 export const globalErrorHandler = ((err: any, req: Request, res: Response, next: NextFunction) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || "Internal server error";
+    let statusCode = err.statusCode || 500;
+    let message = err.message || "Internal server error";
 
-    const error = [
+    let error: TError = [
         {
             path: "",
             message: "",
         },
     ]
+
+    if (err instanceof ZodError) {
+        const formattedZodError = handleZodValidationError(err);
+
+        statusCode = formattedZodError?.statusCode;
+        message = formattedZodError?.message;
+        error = formattedZodError?.error;
+    }
 
     return res.status(statusCode).json({
         success: false,
