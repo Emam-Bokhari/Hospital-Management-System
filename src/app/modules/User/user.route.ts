235251ca6cbe0a14c652e25e7247ad3e760300d4
Moshfiqur Rahman <studentemam@gmail.com>
2025-01-07 21:00:48 +0600

    feat(user): add request validation for user update and creation

--- src/app/modules/User/user.route.ts
+++ src/app/modules/User/user.route.ts
@@ -1,15 +1,17 @@
 import express from 'express';
 import { UserControllers } from './user.controller';
+import { validateRequestSchema } from '../../middlewares/validateRequestSchema';
+import { UserValidationSchema } from './user.validation';
 
 const router = express.Router();
 
-router.post('/', UserControllers.createUserController);
+router.post('/', validateRequestSchema(UserValidationSchema.createUserValidationSchema), UserControllers.createUserController);
 
 router.get('/', UserControllers.getAllUsersController);
 
 router.get('/:id', UserControllers.getUserController);
 
-router.patch('/:id', UserControllers.updateUserController);
+router.patch('/:id', validateRequestSchema(UserValidationSchema.updateUserValidationSchema), UserControllers.updateUserController);
 
 router.delete('/:id', UserControllers.deleteUserController);
 
