ea69ed10cef3e42095cee1524cd22f76dbd7967c
Moshfiqur Rahman <studentemam@gmail.com>
2025-01-09 22:30:46 +0600

    feat(doctor): create  validate time range utility for ensure start time is before end time

--- src/app/modules/Doctor/doctor.utils.ts
+++ src/app/modules/Doctor/doctor.utils.ts
@@ -5,3 +5,13 @@ export const validateDateRange = (startDate: Date, endDate: Date, errorMessage: 
         throw new HttpError(400, errorMessage)
     }
 }
+
+export const validateTimeRange = (startTime: string, endTime: string, errorMessage: string) => {
+    const start = new Date(`1970-01-01T${startTime}:00`);
+
+    const end = new Date(`1970-01-01T${endTime}:00`);
+
+    if (start >= end) {
+        throw new HttpError(400, errorMessage)
+    }
+}
