2ae0c388d39f90de892e923e26d8421919ca6c75
Moshfiqur Rahman <studentemam@gmail.com>
2025-01-13 00:56:53 +0600

    chore: resolve merge conflict problem and update code

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,11 +1,10 @@
 import express from 'express';
 import { UserRoutes } from '../modules/User/user.route';
 import { DoctorRoutes } from '../modules/Doctor/doctor.route';
-<<<<<<< HEAD
+
 import { SpecializationRoutes } from '../modules/Specialization/specialization.route';
-=======
+
 import { DepartmentRoutes } from '../modules/Department/department.route';
->>>>>>> feature/departments-crud
 
 const router = express.Router();
 
@@ -19,13 +18,14 @@ const moduleRoutes = [
     route: DoctorRoutes,
   },
   {
-<<<<<<< HEAD
+
     path: "/specializations",
     route: SpecializationRoutes
-=======
+  },
+  {
     path: "/departments",
     route: DepartmentRoutes
->>>>>>> feature/departments-crud
+
   }
 ];
 
