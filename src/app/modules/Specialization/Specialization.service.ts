fa2b7a99e2f5ced96eb7f8969594fb3198c5545a
Moshfiqur Rahman <studentemam@gmail.com>
2025-01-12 21:33:14 +0600

    feat(specialization): add getAllSpecializations service function

--- src/app/modules/Specialization/Specialization.service.ts
+++ src/app/modules/Specialization/Specialization.service.ts
@@ -1,3 +1,4 @@
+import { HttpError } from "../../errors/HttpError";
 import { TSpecialization } from "./specialization.interface";
 import { Specialization } from "./specialization.model";
 
@@ -7,6 +8,15 @@ const createSpecialization = async (payload: TSpecialization) => {
     return createdSpecialization;
 }
 
+const getAllSpecializations = async () => {
+    const specializations = await Specialization.find();
+    if (!specializations) {
+        throw new HttpError(404, "No specialization were found in the database")
+    }
+    return specializations;
+}
+
 export const SpecializationServices = {
     createSpecialization,
+    getAllSpecializations,
 }
