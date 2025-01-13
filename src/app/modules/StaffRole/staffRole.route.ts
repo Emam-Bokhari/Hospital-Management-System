e43b91a5568f243880e4b13f83a3ea4a8c25b499
Moshfiqur Rahman <studentemam@gmail.com>
2025-01-14 01:24:37 +0600

    feat(staffRole): add validation for staff role name

--- src/app/modules/StaffRole/staffRole.route.ts
+++ src/app/modules/StaffRole/staffRole.route.ts
@@ -1,15 +1,17 @@
 import express from "express";
 import { StaffRoleControllers } from "./staffRole.controller";
+import { validateRequestSchema } from "../../middlewares/validateRequestSchema";
+import { StaffRoleValidationSchema } from "./staffRole.validation";
 
 const router = express.Router();
 
-router.post("/", StaffRoleControllers.createStaffRoleController);
+router.post("/", validateRequestSchema(StaffRoleValidationSchema), StaffRoleControllers.createStaffRoleController);
 
 router.get("/", StaffRoleControllers.getAllStaffRolesController);
 
 router.get("/:id", StaffRoleControllers.getStaffRoleController);
 
-router.patch("/:id", StaffRoleControllers.updateStaffRoleController);
+router.patch("/:id", validateRequestSchema(StaffRoleValidationSchema), StaffRoleControllers.updateStaffRoleController);
 
 router.delete("/:id", StaffRoleControllers.deleteStaffRoleController)
 
