3f58a40eb0d0275f6cd2d83edb8a0c7427c65e30
Moshfiqur Rahman <studentemam@gmail.com>
2025-01-15 22:55:25 +0600

    feat(appointmentBooking): add type define for appointment booking and related fields

---
+++ src/app/modules/AppointmentBooking/appointmentBooking.interface.ts
@@ -0,0 +1,42 @@
+import { Types } from "mongoose"
+
+type TAddress = {
+    division: string;
+    district: string;
+    subDistrict: string;
+}
+
+export type TContactInformation = {
+    phone: string;
+    email?: string;
+};
+
+export type TBloodGroup =
+    | 'A+'
+    | 'A-'
+    | 'B+'
+    | 'B-'
+    | 'AB+'
+    | 'AB-'
+    | 'O+'
+    | 'O-';
+
+export type TAppointmentBooking = {
+    userId?: Types.ObjectId;
+    firstName: string;
+    lastName: string;
+    age: number;
+    weight?: number;
+    bloodGroup?: TBloodGroup;
+    gender: "male" | "female";
+    address: TAddress;
+    contactInformation: TContactInformation;
+    doctorId: Types.ObjectId;
+    dateOfAppointment: Date;
+    timeSlot: string;
+    status?: ["pending", "confirmed", "completed", "cancelled"];
+    prescriptionFiles: string[];
+    testReportFiles: string[];
+    additionalNotes: string;
+    paymentId: Types.ObjectId;
+}
