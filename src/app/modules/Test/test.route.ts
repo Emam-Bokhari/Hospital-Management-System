9727fe89e01c8b38e1774ba2fc35b261ebebf89e
Moshfiqur Rahman <studentemam@gmail.com>
2025-01-15 16:00:23 +0600

    feat(test): add Zod validation for creating and updating test

--- src/app/modules/Test/test.route.ts
+++ src/app/modules/Test/test.route.ts
@@ -1,17 +1,19 @@
 import express from "express";
 import { TestControllers } from "./test.controller";
+import { validateRequestSchema } from "../../middlewares/validateRequestSchema";
+import { TestValidationSchema } from "./test.validation";
 
 const router = express.Router();
 
-router.post("/", TestControllers.createTestController);
+router.post("/", validateRequestSchema(TestValidationSchema.createTestValidationSchema), TestControllers.createTestController);
 
 router.get("/", TestControllers.getAllTestsController);
 
 router.get("/:id", TestControllers.getTestController);
 
-router.patch("/:id", TestControllers.updateTestController);
+router.patch("/:id", validateRequestSchema(TestValidationSchema.updateTestValidationSchema), TestControllers.updateTestController);
 
-router.patch("/:id/availability", TestControllers.updateTestAvailabilityController)
+router.patch("/:id/availability", validateRequestSchema(TestValidationSchema.testAvailabilityUpdateValidationSchema), TestControllers.updateTestAvailabilityController)
 
 router.delete("/:id", TestControllers.deleteTestController)
 
