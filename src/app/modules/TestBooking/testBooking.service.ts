e5901ca583fa9fde033e6977dbd80f2caab7c437
Moshfiqur Rahman <studentemam@gmail.com>
2025-01-18 23:40:26 +0600

    feat(testBooking): implement status validation in updateTestBookingStatusById service

--- src/app/modules/TestBooking/testBooking.service.ts
+++ src/app/modules/TestBooking/testBooking.service.ts
@@ -1,8 +1,19 @@
 import { HttpError } from "../../errors/HttpError";
+import { Test } from "../Test/test.model";
 import { TTestBooking } from "./testBooking.interface";
 import { TestBooking } from "./testBooking.model";
 
 const createTestBooking = async (payload: TTestBooking) => {
+
+    const test = await Test.findOne({ _id: payload.test });
+
+    if (!test) {
+        throw new HttpError(404, "The requested test does not exist.");
+    }
+
+    if (!test?.testAvailability) {
+        throw new HttpError(400, "The requested test is currently unavailable. Please choose a different test or try again later.")
+    }
 
     // TODO: automatic set userId
 
@@ -34,8 +45,29 @@ const getTestBookingById = async (id: string) => {
     return testBooking;
 }
 
+const updateTestBookingStatusById = async (id: string, status: string) => {
+
+    const validStatuses = ['pending', 'confirmed', 'completed', 'cancelled']
+
+    if (!validStatuses.includes(status)) {
+        throw new HttpError(400, `Invalid status: ${status}`)
+    }
+
+    // TODO: payment related validation
+
+    const updatedTestBookingStatus = await TestBooking.findOneAndUpdate({ _id: id }, { status }, { new: true, runValidators: true })
+
+    if (!updatedTestBookingStatus) {
+        throw new HttpError(404, `Test booking with ID: ${id} not found`)
+    }
+
+    return updatedTestBookingStatus;
+
+}
+
 export const TestBookingServices = {
     createTestBooking,
     getAllTestBookings,
     getTestBookingById,
+    updateTestBookingStatusById,
 }
