6c08fd00ba831a14a4163ca1ff4939dba7486bf8
Moshfiqur Rahman <studentemam@gmail.com>
2025-01-20 00:51:30 +0600

    feat(appointmentBooking):add populate and selected only necessary fields

--- src/app/modules/AppointmentBooking/appointmentBooking.service.ts
+++ src/app/modules/AppointmentBooking/appointmentBooking.service.ts
@@ -98,7 +98,9 @@ const createAppointmentBooking = async (payload: TAppointmentBooking) => {
 };
 
 const getAllAppointmentBookings = async () => {
-  const appointmentBookings = await AppointmentBooking.find();
+  const appointmentBookings = await AppointmentBooking.find()
+    .populate({ path: "userId" })
+    .populate({ path: "doctor", select: "firstName lastName", populate: ({ path: "specialization", select: "name" }) });
 
   if (appointmentBookings.length === 0) {
     throw new HttpError(
@@ -111,7 +113,8 @@ const getAllAppointmentBookings = async () => {
 };
 
 const getAppointmentBookingById = async (id: string) => {
-  const appointmentBooking = await AppointmentBooking.findById(id);
+  const appointmentBooking = await AppointmentBooking.findById(id).populate({ path: "userId" })
+    .populate({ path: "doctor", select: "firstName lastName", populate: ({ path: "specialization", select: "name" }) });;
 
   if (!appointmentBooking) {
     throw new HttpError(404, `No appointment booking found with ID: ${id}`);
