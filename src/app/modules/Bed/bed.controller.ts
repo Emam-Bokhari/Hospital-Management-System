d7ceeb397836a92323dba476fb17a4129bfc2f57
Moshfiqur Rahman <studentemam@gmail.com>
2025-01-20 23:42:48 +0600

    feat(bed): implement controller for creating a bed

---
+++ src/app/modules/Bed/bed.controller.ts
@@ -0,0 +1,19 @@
+import { asyncHandler } from "../../utils/global/asyncHandler";
+import { sendResponse } from "../../utils/global/sendResponse";
+import { BedServices } from "./bed.service";
+
+const createBedController = asyncHandler(async (req, res) => {
+    const bedPayload = req.body;
+    const createdBed = await BedServices.createBed(bedPayload);
+
+    sendResponse(res, {
+        success: true,
+        message: "Bed created successfully",
+        statusCode: 201,
+        data: createdBed,
+    })
+})
+
+export const BedControllers = {
+    createBedController,
+}
