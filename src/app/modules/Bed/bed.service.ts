4b6551308b1f16cdf842620d8ef5db114828cce2
Moshfiqur Rahman <studentemam@gmail.com>
2025-01-20 23:24:08 +0600

    feat(bed): add getBedById function to fetch a bed data by ID

--- src/app/modules/Bed/bed.service.ts
+++ src/app/modules/Bed/bed.service.ts
@@ -17,7 +17,18 @@ const getAllBeds = async () => {
     return beds;
 }
 
+const getBedById = async (id: string) => {
+    const bed = await Bed.findById(id);
+
+    if (!bed) {
+        throw new HttpError(404, `No bed found with ID: ${id}`)
+    };
+
+    return bed;
+}
+
 export const BedServices = {
     createBed,
     getAllBeds,
+    getBedById,
 }
