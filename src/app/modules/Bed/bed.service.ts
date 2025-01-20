0a3cc1520cdbe351f9bd4450a4a42147763f33a6
Moshfiqur Rahman <studentemam@gmail.com>
2025-01-20 23:22:19 +0600

    feat(bed): add getAllBeds function to retrieving all beds

--- src/app/modules/Bed/bed.service.ts
+++ src/app/modules/Bed/bed.service.ts
@@ -1,3 +1,4 @@
+import { HttpError } from "../../errors/HttpError";
 import { TBed } from "./bed.interface";
 import { Bed } from "./bed.model";
 
@@ -6,6 +7,17 @@ const createBed = async (payload: TBed) => {
     return createdBed;
 };
 
+const getAllBeds = async () => {
+    const beds = await Bed.find();
+
+    if (beds.length === 0) {
+        throw new HttpError(404, "No bed were found in the database")
+    }
+
+    return beds;
+}
+
 export const BedServices = {
     createBed,
+    getAllBeds,
 }
