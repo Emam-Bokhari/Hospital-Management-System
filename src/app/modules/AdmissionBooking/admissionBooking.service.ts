fab5ab243406fb095b03efacefe9be9265eaf7f1
Moshfiqur Rahman <studentemam@gmail.com>
2025-02-24 08:58:08 +0600

    feat(admissionBooking): createAdmissionBooking service function

--- src/app/modules/AdmissionBooking/admissionBooking.service.ts
+++ src/app/modules/AdmissionBooking/admissionBooking.service.ts
@@ -1,13 +1,34 @@
 import { HttpError } from "../../errors/HttpError";
 import { Bed } from "../Bed/bed.model";
 import { TAdmissionBooking } from "./admissionBooking.interface";
+import { AdmissionBooking } from "./admissionBooking.model";
+import { generateAdmissionBookingId } from "./admissionBooking.utils";
 
 const createAdmissionBooking = async (payload: TAdmissionBooking) => {
     const bed = await Bed.findById(payload?.bed);
     // check if bed is exists
     if (!bed) {
-        throw new HttpError(404, "The requested bed does not exist.");
+        throw new HttpError(404, "The specified bed could not be found.");
     }
 
+    // check if bed is available
+    if (bed.availabilityStatus && ["occupied", "maintenance"].includes(bed.availabilityStatus)) {
+        const statusMessage = bed.availabilityStatus === "occupied"
+            ? "The selected bed is currently occupied."
+            : "The selected bed is under maintenance.";
+        throw new HttpError(400, statusMessage);
+    }
 
+    // generate admission booking id
+    const admissionBookingId = await generateAdmissionBookingId();
+    payload.id = admissionBookingId;
+
+    const createdAdmissionBooking = await AdmissionBooking.create(payload)
+
+    return createdAdmissionBooking;
+
+}
+
+export const AdmissionBookingServices = {
+    createAdmissionBooking,
 }
---
+++ src/app/modules/AdmissionBooking/admissionBooking.utils.ts
@@ -0,0 +1,8 @@
+import { generateDynamicId } from '../../utils/modelSpecific/generateDynamicId';
+import { AdmissionBooking } from './admissionBooking.model';
+
+
+export const generateAdmissionBookingId = async () => {
+    const admissionBookingId = await generateDynamicId(AdmissionBooking, 'ADM');
+    return admissionBookingId;
+};
