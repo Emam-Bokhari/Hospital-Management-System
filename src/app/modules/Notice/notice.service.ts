8f606c830b1044d0e8d56b493e49329b66e6a5b0
Moshfiqur Rahman <studentemam@gmail.com>
2025-03-16 12:39:40 +0600

    feat(notice): add getAllNotices service

--- src/app/modules/Notice/notice.service.ts
+++ src/app/modules/Notice/notice.service.ts
@@ -1,9 +1,20 @@
+import { HttpError } from "../../errors/HttpError";
 import { TNotice } from "./notice.interface";
 import { Notice } from "./notice.model";
 
 const createNotice = async (payload: TNotice) => {
     const createdNotice = await Notice.create(payload);
     return createdNotice;
+}
+
+export const getAllNotices = async () => {
+    const notices = await Notice.find();
+
+    if (notices.length === 0) {
+        throw new HttpError(404, "No notice were found in the database")
+    }
+
+    return notices;
 }
 
 export const NoticeServices = {
