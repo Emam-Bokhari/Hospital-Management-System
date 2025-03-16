3214144cdf72adecce0e9a8126487abcffa0f52a
Moshfiqur Rahman <studentemam@gmail.com>
2025-03-16 20:04:58 +0600

    feat(review): add createReviewController to handle review creation

---
+++ src/app/modules/Review/review.controller.ts
@@ -0,0 +1,19 @@
+import { asyncHandler } from "../../utils/global/asyncHandler";
+import { sendResponse } from "../../utils/global/sendResponse";
+import { ReviewServices } from "./review.service";
+
+const createReviewController = asyncHandler(async (req, res) => {
+    const reviewPayload = req.body;
+    const createdReview = await ReviewServices.createReview(reviewPayload);
+
+    sendResponse(res, {
+        success: true,
+        message: 'Review created successfully',
+        statusCode: 201,
+        data: createdReview,
+    });
+});
+
+export const ReviewControllers = {
+    createReviewController,
+}
