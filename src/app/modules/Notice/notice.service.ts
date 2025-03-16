705b1e82209d14773c9a5f381356156a681e513a
Moshfiqur Rahman <studentemam@gmail.com>
2025-03-16 12:43:32 +0600

    feat(notice): add deleteNoticeById service

--- src/app/modules/Notice/notice.service.ts
+++ src/app/modules/Notice/notice.service.ts
@@ -30,8 +30,22 @@ export const getNoticeById = async (id: string) => {
     return notice;
 }
 
+const deleteNoticeById = async (id: string) => {
+    const deletedNotice = await Notice.findOneAndUpdate(
+        { _id: id, isDeleted: false },
+        { isDeleted: true },
+        { new: true },
+    );
+    if (!deletedNotice) {
+        throw new HttpError(404, `No notice found with ID: ${id}`);
+    }
+    return deletedNotice;
+};
+
+
 export const NoticeServices = {
     createNotice,
     getAllNotices,
     getNoticeById,
+    deleteNoticeById,
 }
