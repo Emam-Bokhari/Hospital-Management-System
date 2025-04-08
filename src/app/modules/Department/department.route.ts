f15a92a4c31b95463d8df286694db764bbaf648b
Moshfiqur Rahman <studentemam@gmail.com>
2025-04-08 13:49:47 +0600

    feat(auth): add auth guard for department

--- src/app/modules/Department/department.route.ts
+++ src/app/modules/Department/department.route.ts
@@ -2,11 +2,14 @@ import express from 'express';
 import { DepartmentControllers } from './department.controller';
 import { validateRequestSchema } from '../../middlewares/validateRequestSchema';
 import { DepartmentValidationSchema } from './department.validation';
+import { auth } from '../../middlewares/auth';
+import { USER_ROLE } from '../User/user.constant';
 
 const router = express.Router();
 
 router.post(
   '/',
+  auth(USER_ROLE.admin, USER_ROLE.superAdmin),
   validateRequestSchema(
     DepartmentValidationSchema.createDepartmentValidationSchema,
   ),
@@ -17,6 +20,6 @@ router.get('/', DepartmentControllers.getAllDepartmentsController);
 
 router.get('/:id', DepartmentControllers.getDepartmentController);
 
-router.delete('/:id', DepartmentControllers.deleteDepartmentController);
+router.delete('/:id', auth(USER_ROLE.admin, USER_ROLE.superAdmin), DepartmentControllers.deleteDepartmentController);
 
 export const DepartmentRoutes = router;
