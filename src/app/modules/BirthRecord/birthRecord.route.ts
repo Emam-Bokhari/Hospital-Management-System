1e27dcebe4f94e46c766d4ec214a3d9c0a7125f8
Moshfiqur Rahman <studentemam@gmail.com>
2025-04-08 13:48:29 +0600

    feat(auth): add auth guard for birth and death record

--- src/app/modules/BirthRecord/birthRecord.route.ts
+++ src/app/modules/BirthRecord/birthRecord.route.ts
@@ -1,17 +1,18 @@
 import express from 'express';
 import { BirthRecordControllers } from './birthRecord.controller';
 import { auth } from '../../middlewares/auth';
+import { USER_ROLE } from '../User/user.constant';
 
 const router = express.Router();
 
-router.post('/', BirthRecordControllers.createBirthRecordController);
+router.post('/', auth(USER_ROLE.receptionist, USER_ROLE.admin, USER_ROLE.superAdmin), BirthRecordControllers.createBirthRecordController);
 
-router.get('/', auth(), BirthRecordControllers.getAllBirthRecordsController);
+router.get('/', auth(USER_ROLE.receptionist, USER_ROLE.admin, USER_ROLE.superAdmin), BirthRecordControllers.getAllBirthRecordsController);
 
-router.get('/:id', BirthRecordControllers.getBirthRecordController);
+router.get('/:id', auth(USER_ROLE.receptionist, USER_ROLE.admin, USER_ROLE.superAdmin), BirthRecordControllers.getBirthRecordController);
 
-router.patch('/:id', BirthRecordControllers.updateBirthRecordController);
+router.patch('/:id', auth(USER_ROLE.receptionist, USER_ROLE.admin, USER_ROLE.superAdmin), BirthRecordControllers.updateBirthRecordController);
 
-router.delete('/:id', BirthRecordControllers.deleteBirthRecordController);
+router.delete('/:id', auth(USER_ROLE.receptionist, USER_ROLE.admin, USER_ROLE.superAdmin), BirthRecordControllers.deleteBirthRecordController);
 
 export const BirthRecordRoutes = router;
