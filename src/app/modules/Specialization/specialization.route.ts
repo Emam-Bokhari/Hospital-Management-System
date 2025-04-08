f09f6910bdf78c4ff25d4c78d46a6d97e808b1cb
Moshfiqur Rahman <studentemam@gmail.com>
2025-04-08 13:57:36 +0600

    feat(auth): add auth guard for review and specialization

--- src/app/modules/Specialization/specialization.route.ts
+++ src/app/modules/Specialization/specialization.route.ts
@@ -2,11 +2,14 @@ import express from 'express';
 import { SpecializationControllers } from './specialization.controller';
 import { validateRequestSchema } from '../../middlewares/validateRequestSchema';
 import { SpecializationValidationSchema } from './specialization.validation';
+import { auth } from '../../middlewares/auth';
+import { USER_ROLE } from '../User/user.constant';
 
 const router = express.Router();
 
 router.post(
   '/',
+  auth(USER_ROLE.admin, USER_ROLE.superAdmin),
   validateRequestSchema(
     SpecializationValidationSchema.createSpecializationValidationSchema,
   ),
@@ -19,12 +22,13 @@ router.get('/:id', SpecializationControllers.getSpecializationController);
 
 router.patch(
   '/:id',
+  auth(USER_ROLE.admin, USER_ROLE.superAdmin),
   validateRequestSchema(
     SpecializationValidationSchema.createSpecializationValidationSchema,
   ),
   SpecializationControllers.updateSpecializationController,
 );
 
-router.delete('/:id', SpecializationControllers.deleteSpecializationController);
+router.delete('/:id', auth(USER_ROLE.admin, USER_ROLE.superAdmin), SpecializationControllers.deleteSpecializationController);
 
 export const SpecializationRoutes = router;
