5bf046f939f7ec4fbba0e9f95f2d0ca0f1a3fe32
Moshfiqur Rahman <studentemam@gmail.com>
2025-04-08 13:59:28 +0600

    feat(auth): add auth guard for staff and staff role

--- src/app/modules/Staff/staff.route.ts
+++ src/app/modules/Staff/staff.route.ts
@@ -2,25 +2,29 @@ import express from 'express';
 import { StaffControllers } from './staff.controller';
 import { validateRequestSchema } from '../../middlewares/validateRequestSchema';
 import { StaffValidationSchema } from './staff.validation';
+import { auth } from '../../middlewares/auth';
+import { USER_ROLE } from '../User/user.constant';
 
 const router = express.Router();
 
 router.post(
   '/',
+  auth(USER_ROLE.admin, USER_ROLE.superAdmin),
   validateRequestSchema(StaffValidationSchema.createStaffValidationSchema),
   StaffControllers.createStaffController,
 );
 
-router.get('/', StaffControllers.getAllStaffsController);
+router.get('/', auth(USER_ROLE.admin, USER_ROLE.superAdmin), StaffControllers.getAllStaffsController);
 
-router.get('/:id', StaffControllers.getStaffController);
+router.get('/:id', auth(USER_ROLE.admin, USER_ROLE.superAdmin), StaffControllers.getStaffController);
 
 router.patch(
   '/:id',
+  auth(USER_ROLE.admin, USER_ROLE.superAdmin),
   validateRequestSchema(StaffValidationSchema.updateStaffValidationSchema),
   StaffControllers.updateStaffController,
 );
 
-router.delete('/:id', StaffControllers.deleteStaffController);
+router.delete('/:id', auth(USER_ROLE.admin, USER_ROLE.superAdmin), StaffControllers.deleteStaffController);
 
 export const StaffRoutes = router;
