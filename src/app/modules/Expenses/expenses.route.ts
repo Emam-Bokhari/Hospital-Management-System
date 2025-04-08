7647f542a77a6bde3a89cc8b8e6b3716eae480e2
Moshfiqur Rahman <studentemam@gmail.com>
2025-04-08 13:53:28 +0600

    feat(auth): add auth guard for doctor and expenses

--- src/app/modules/Expenses/expenses.route.ts
+++ src/app/modules/Expenses/expenses.route.ts
@@ -1,16 +1,18 @@
 import express from "express";
 import { ExpensesControllers } from "./expenses.controller";
+import { auth } from "../../middlewares/auth";
+import { USER_ROLE } from "../User/user.constant";
 
 const router = express.Router();
 
-router.post("/", ExpensesControllers.createExpensesController);
+router.post("/", auth(USER_ROLE.financeManager, USER_ROLE.admin, USER_ROLE.superAdmin), ExpensesControllers.createExpensesController);
 
-router.get("/", ExpensesControllers.getAllExpenses);
+router.get("/", auth(USER_ROLE.financeManager, USER_ROLE.admin, USER_ROLE.superAdmin), ExpensesControllers.getAllExpenses);
 
-router.get("/type/:type", ExpensesControllers.getExpensesByType);
+router.get("/type/:type", auth(USER_ROLE.financeManager, USER_ROLE.admin, USER_ROLE.superAdmin), ExpensesControllers.getExpensesByType);
 
-router.get("/:id", ExpensesControllers.getExpensesById);
+router.get("/:id", auth(USER_ROLE.financeManager, USER_ROLE.admin, USER_ROLE.superAdmin), ExpensesControllers.getExpensesById);
 
-router.delete("/:id", ExpensesControllers.deleteExpensesById);
+router.delete("/:id", auth(USER_ROLE.financeManager, USER_ROLE.admin, USER_ROLE.superAdmin), ExpensesControllers.deleteExpensesById);
 
 export const ExpensesRoutes = router;
