c1b61de81937b61e24678fb44499acf127787a70
Moshfiqur Rahman <studentemam@gmail.com>
2025-04-08 14:07:40 +0600

    feat(auth): add auth guard for user

--- src/app/modules/User/user.route.ts
+++ src/app/modules/User/user.route.ts
@@ -2,12 +2,14 @@ import express from 'express';
 import { UserControllers } from './user.controller';
 import { validateRequestSchema } from '../../middlewares/validateRequestSchema';
 import { UserValidationSchema } from './user.validation';
+import { USER_ROLE } from './user.constant';
+import { auth } from '../../middlewares/auth';
 
 const router = express.Router();
 
-router.get('/', UserControllers.getAllUsersController);
+router.get('/', auth(USER_ROLE.receptionist, USER_ROLE.admin, USER_ROLE.superAdmin), UserControllers.getAllUsersController);
 
-router.get('/:id', UserControllers.getUserController);
+router.get('/:id', auth(USER_ROLE.receptionist, USER_ROLE.admin, USER_ROLE.superAdmin), UserControllers.getUserController);
 
 router.patch(
   '/:id',
@@ -15,6 +17,6 @@ router.patch(
   UserControllers.updateUserController,
 );
 
-router.delete('/:id', UserControllers.deleteUserController);
+router.delete('/:id', auth(USER_ROLE.admin, USER_ROLE.superAdmin), UserControllers.deleteUserController);
 
 export const UserRoutes = router;
